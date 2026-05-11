75a22c81a93261246c18c39a9132e707ae7c1b13
I1HSA <caiocaiohen@gmail.com>
2026-05-11 16:20:31 -0300

    ex3

--- script.js
+++ script.js
@@ -1,7 +1,14 @@
-let soma = 0;
+let num = Number(prompt("Digite um número e eu direi a tabuada dele de 1 a 10:"));
+while (isNaN(num)) {
+  num = parseInt(Number(prompt("Resposta inválida! Digite um número e eu direi a tabuada dele de 1 a 10:")));
+}
+
+let mensagem = "";
 
 for (let i = 1; i <= 10; i++) {
-  soma += i;
+  mensagem += `${num*i} `;
   }
 
-alert(`${soma}`);
+  
+
+alert(`${mensagem}`);
