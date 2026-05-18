ce66363397fdba75cc31f761917b13edb23c6516
I1HSA <caiocaiohen@gmail.com>
2026-05-18 09:34:05 -0300

    Exercício 4 concertos.

--- script.js
+++ script.js
@@ -4,15 +4,18 @@ while(menuOn == true){
   resposta = Number(prompt(`1 - Inserir produto
 2 - Listar produtos
 3 - Sair)`));
-  if (resposta == 3) {
+switch (resposta) {
+  case 3:
     menuOn=false;
-}
-  if (resposta == 1){
+    break;
+  case 2:
+    alert("Você escolheu listar produto");
+    resposta = 0;
+    break;
+  case 1:
     alert("Você escolheu inserir produto");
     resposta = 0;
-  }
-  if(resposta ==2){
-    alert("Você escolheu listar produto");
-    reposta = 0;
-  }
+  default:
+    break;
+}
 }
