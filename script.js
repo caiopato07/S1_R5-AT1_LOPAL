fa8321a97bcbd58ce840a2524667116eb9d13148
I1HSA <caiocaiohen@gmail.com>
2026-05-18 08:14:23 -0300

    Concerto do ex. 2

--- script.js
+++ script.js
@@ -1,7 +1,8 @@
 let soma = 0;
-
+let mensagem = "";
 for (let i = 1; i <= 10; i++) {
   soma += i;
+  mensagem += `${soma-i}+${i} = ${soma}.     `
   }
-
-alert(`${soma}`);
+alert(`${mensagem}`);
+alert(`Portanto, o resultado dessas somas no final é ${soma}.`);
